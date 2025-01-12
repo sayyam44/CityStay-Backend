f5409891c4cdfbb113ab898e6e9a1e8628d29a4b
TicTacToe <sayyamkundra52@gmail.com>
2025-01-11 23:58:16 -0330

    11janbackend

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -1,9 +1,24 @@
 from rest_framework import serializers
 from users.models import Profile
+#here we are importing the listing model to calculate the number of 
+# listings for each user
+from listings.models import Listing 
+#here we are serializing the above listing model using the 
+#already created listing_serailizer
+from listings.api.serializers import ListingSerializer
 
 # to serialize the profile models into the readable format
 class ProfileSerailizer(serializers.ModelSerializer):
-    class Meta:
+
+   #this is to get each listing of the seller along with the 
+   # profile model using the listing model and then serializing it
+    seller_listings = serializers.SerializerMethodField()
+    def get_seller_listings(self,obj):
+        query = Listing.objects.filter(seller=obj.seller); #this holds all the listings of the current seller
+        listings_serialized=ListingSerializer(query, many=True) #this serialized the listing model
+        return listings_serialized.data
+    
+    class Meta:#this holds all the data of the profile in models.py
         model=Profile
         fields = "__all__"
 
