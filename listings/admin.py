f6abc4041b389355f6d0fd4e754defff65b20788
TicTacToe <sayyamkundra52@gmail.com>
2025-01-15 20:33:22 -0330

    15thjanback

--- listings/admin.py
+++ listings/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from listings.models import Listing 
-# from .forms import ListingsForm
+from listings.models import Poi
+#registering the new models created in models.py
+from .forms import PoisForm
 
-#this is because we cannot directly register the models that are crreated in forms.py file
-# class ListingAdmin(admin.ModelAdmin):
-#     form=ListingsForm
+#this is because we cannot directly register the models 
+#that we created in forms.py file
+class PoiAdmin(admin.ModelAdmin):
+    form=PoisForm
+# register Poi model and PoiAdmin
+admin.site.register(Poi,PoiAdmin)
 
-# admin.site.register(Listing,ListingAdmin)
+#register the listing model
 admin.site.register(Listing)
