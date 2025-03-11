98643a42d46ad4955c836e82dffe5b2eb5ef2fa6
TicTacToe <sayyamkundra52@gmail.com>
2025-03-11 17:06:01 -0230

    storages_settings

--- citystaybackend/settings.py
+++ citystaybackend/settings.py
@@ -164,19 +164,6 @@ DJOSER = {
     'SEND_ACTIVATION_EMAIL' : False,
 }
 
-AWS_ACCESS_KEY_ID = 'DO801PFMJMP3RQENL3ZY'
-AWS_SECRET_ACCESS_KEY = 'SkhTT6a2dz14bPO/JP5hil05UAOFFO2A+czV69HTtxM'
-AWS_STORAGE_BUCKET_NAME = 'citystaynl-space'
-AWS_S3_ENDPOINT_URL = 'https://citystaynl-space.tor1.digitaloceanspaces.com'
-AWS_S3_OBJECT_PARAMETERS = {
-        'CacheControl' : 'max-age=86400',
-        }
-AWS_LOCATION = 'citystay-bucket-new'
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_S3_SIGNATURE_VERSION = 's3v4'
-
 # If a filename .server_settings exists in this folder then 
 # import everything from it .
 try:
