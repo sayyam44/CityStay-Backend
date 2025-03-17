d9bf3b6b55e65127b2bb8f5023578f0a8ae58516
TicTacToe <sayyamkundra52@gmail.com>
2025-03-17 13:00:50 -0230

    cors

--- citystaybackend/settings.py
+++ citystaybackend/settings.py
@@ -65,6 +65,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'citystaybackend.urls'
