86401758a0f9f6c8b991af3b9f9e28bab1f9ea0c
TicTacToe <sayyamkundra52@gmail.com>
2025-03-29 21:56:58 -0230

    signals.py

--- users/signals.py
+++ users/signals.py
@@ -2,9 +2,6 @@ from .models import Profile
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver 
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
-from django.conf import settings 
 
 User = get_user_model()
 # in models.py file we originally have 2 models 1)usermodel 2)profile model
@@ -24,25 +21,6 @@ def create_user_profile(sender, instance, created, **kwargs):
         # for that newly created user
         Profile.objects.create(seller=instance) 
 
-        subject = "Welcome to CityStayNL!"
-        body = f"Hi , Welcome to CityStayNL! We're excited to have you on board."
-        from_email = "admin@citystaynl.com"  # You can set this as your 'DEFAULT_FROM_EMAIL'
-
-        # Send email using SendGrid
-        message = Mail(
-            from_email=from_email,
-            to_emails=instance.email,
-            subject=subject,
-            plain_text_content=body
-        )
-
-        try:
-            # Use the SendGrid API key from your settings
-            sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
-            response = sg.send(message)
-            print(f"Email sent with status code: {response.status_code}")
-        except Exception as e:
-            print(f"Error sending email: {e}")
 
 # above function creates the profile now the below function saves 
 # this profile
